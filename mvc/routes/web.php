5b48c6d8dbfb9dbfbfe5d267b750f67bb8f74c9d
EUNICEEE28 <kiarabautista38@gmail.com>
2026-07-07 11:41:22 -0600

    Refactor routes to return JSON responses

--- mvc/routes/web.php
+++ mvc/routes/web.php
@@ -3,15 +3,28 @@
 use Lib\Route;
 
 Route::get('', function() {
-    echo "hola desde la página principal";
+    return [
+        'message' => 'hola desde la página principal',
+        'status' => 200
+    ];
 });
 
 Route::get('/contact', function() {
-    echo "hola desde la página contacto";
+    return "hola desde la página contacto";
 });
 
 Route::get('/about', function() {
-    echo "hola desde la página acerca de";
+    return "hola desde la página acerca de";
 });
+
+Route::get('/courses/prueba', function() {
+    return "hola desde la página cursos de prueba";
+});
+
+Route::get('/courses/:slug', function($slug) {
+    return "El curso es: " . $slug;
+});
+
+
 
 Route::dispatch();
